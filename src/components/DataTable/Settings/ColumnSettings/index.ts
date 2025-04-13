76dcdde137f55006fbb303f0c03cc0341daf0366
Anand Rao <nndrao@gmail.com>
2025-04-12 22:30:14 -0400

    removed files

--- src/components/DataTable/Settings/ColumnSettings/index.ts
+++
@@ -1,8 +0,0 @@
-export { SettingsSection } from './SettingsSection';
-export { SettingsItem } from './SettingsItem';
-export { FormattingSettings } from './FormattingSettings';
-export { StyleSettings } from './StyleSettings';
-export { FilterSettings } from './FilterSettings';
-export { ComponentSettings } from './ComponentSettings';
-export { BorderStyleEditor } from './BorderStyleEditor';
-export * from './types'; 
